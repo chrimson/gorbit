997d6eb2648d9e8f0216b5f353d448088d7c0806
Chris Limson <chrimson@gmail.com>
2024-09-03 15:17:58 -0400

    Add sun, fix lights

--- main.go
+++ main.go
@@ -20,60 +20,58 @@ import (
 )
 
 func main() {
-	// Create application and scene
 	a := app.App()
-	systemOrbits := core.NewNode()
-	earthOrbit := core.NewNode()
+	system := core.NewNode()
 
-	// Create perspective camera
 	cam := camera.New(1)
-	cam.SetPosition(0, 0, 10)
-	systemOrbits.Add(cam)
-
-	// Set up orbit control for the camera
+	cam.SetPosition(0, 7, 15)
+	cam.LookAt(&math32.Vector3{X: 0.0, Y: 0.0, Z: 0.0}, &math32.Vector3{X: 0.0, Y: 10.0, Z: 10.0})
 	camera.NewOrbitControl(cam)
+	system.Add(cam)
 
-	// Set up callback to update viewport and camera aspect ratio when the window is resized
 	onResize := func(evname string, ev interface{}) {
-		// Get framebuffer size and update viewport accordingly
 		width, height := a.GetSize()
 		a.Gls().Viewport(0, 0, int32(width), int32(height))
-		// Update the camera's aspect ratio
 		cam.SetAspect(float32(width) / float32(height))
 	}
 	a.Subscribe(window.OnWindowSize, onResize)
 	onResize("", nil)
 
-	earthShape := geometry.NewSphere(0.5, 360, 360)
+	sunShape := geometry.NewSphere(2, 360, 360)
+	sunColor := material.NewStandard(&math32.Color{R: 1.0, G: 0.9, B: 0.7})
+	sunObj := graphic.NewMesh(sunShape, sunColor)
+	sun := core.NewNode()
+	sun.Add(sunObj)
+	sun.Add(light.NewAmbient(&math32.Color{R: 1.0, G: 1.0, B: 1.0}, 0.8))
+	system.Add(sun)
 
+	earthShape := geometry.NewSphere(0.5, 360, 360)
 	earthImage := func(path string) *texture.Texture2D {
 		t, _ := texture.NewTexture2DFromImage(path)
 		t.SetFlipY(false)
 		return t
 	}
 	earthTexture := material.NewStandard(&math32.Color{R: 1.0, G: 1.0, B: 1.0})
-	earthTexture.SetShininess(1)
+	earthTexture.SetShininess(10)
 	earthTexture.AddTexture(earthImage("earth_clouds_big.jpg"))
+	earthObj := graphic.NewMesh(earthShape, earthTexture)
+	earthObj.TranslateX(10.0)
+	earthObj.RotateZ(23.4 * math32.Pi / 180)
+	earth := core.NewNode()
+	earth.Add(earthObj)
+	system.Add(earth)
 
-	earth := graphic.NewMesh(earthShape, earthTexture)
-	earth.TranslateX(5.0)
-	earth.RotateZ(0.4084)
-	earthOrbit.Add(earth)
-	systemOrbits.Add(earthOrbit)
+	lights := light.NewPoint(&math32.Color{R: 1, G: 1, B: 1}, 20.0)
+	lights.SetPosition(0, 0, 0)
+	system.Add(lights)
 
-	systemOrbits.Add(light.NewAmbient(&math32.Color{R: 1.0, G: 1.0, B: 1.0}, 0.2))
-	sunLight := light.NewPoint(&math32.Color{R: 1, G: 1, B: 1}, 20.0)
-	sunLight.SetPosition(0, 0, 0)
-	systemOrbits.Add(sunLight)
-
-	// Run the application
 	a.Run(func(renderer *renderer.Renderer, deltaTime time.Duration) {
 		a.Gls().Clear(gls.DEPTH_BUFFER_BIT | gls.STENCIL_BUFFER_BIT | gls.COLOR_BUFFER_BIT)
 
-		delta := float32(deltaTime.Seconds()) * 2 * math32.Pi / 20
-		earthOrbit.RotateY(delta)
-		earth.RotateY(delta * 10)
+		delta := float32(deltaTime.Seconds()) * 2 * math32.Pi / 365
+		earth.RotateY(delta)
+		earthObj.RotateY(-delta * 365)
 
-		renderer.Render(systemOrbits, cam)
+		renderer.Render(system, cam)
 	})
 }
