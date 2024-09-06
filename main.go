4ab0ed9e4a2286325b41a0198a07e5b8edf71c2b
Chris Limson <chrimson@gmail.com>
2024-09-06 13:43:47 -0400

    Control

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/g3n/engine/app"
@@ -9,14 +10,14 @@ import (
 	"github.com/g3n/engine/geometry"
 	"github.com/g3n/engine/gls"
 	"github.com/g3n/engine/graphic"
+	"github.com/g3n/engine/gui"
 	"github.com/g3n/engine/texture"
+	"github.com/g3n/engine/window"
 
 	"github.com/g3n/engine/light"
 	"github.com/g3n/engine/material"
 	"github.com/g3n/engine/math32"
 	"github.com/g3n/engine/renderer"
-
-	"github.com/g3n/engine/window"
 )
 
 func main() {
@@ -37,12 +38,22 @@ func main() {
 	a.Subscribe(window.OnWindowSize, onResize)
 	onResize("", nil)
 
+	s1 := gui.NewHSlider(400, 32)
+	s1.SetPosition(10, 10)
+	s1.SetValue(0.2)
+	s1.SetText(fmt.Sprintf("%1.2f", s1.Value()))
+	s1.Subscribe(gui.OnChange, func(evname string, ev interface{}) {
+		// a.Log().Info("Slider 1 OnChange: %v", s1.Value())
+		s1.SetText(fmt.Sprintf("%1.2f", s1.Value()))
+	})
+	system.Add(s1)
+
 	sunShape := geometry.NewSphere(2, 360, 360)
 	sunColor := material.NewStandard(&math32.Color{R: 1.0, G: 0.9, B: 0.7})
 	sunObj := graphic.NewMesh(sunShape, sunColor)
 	sun := core.NewNode()
 	sun.Add(sunObj)
-	sun.Add(light.NewAmbient(&math32.Color{R: 1.0, G: 1.0, B: 1.0}, 0.8))
+	sun.Add(light.NewAmbient(&math32.Color{R: 1.0, G: 1.0, B: 1.0}, 0.3))
 	system.Add(sun)
 
 	earthShape := geometry.NewSphere(0.5, 360, 360)
@@ -124,8 +135,8 @@ func main() {
 	earthPath := graphic.NewLineStrip(earthPathCircle, earthPathMaterial)
 	system.Add(earthPath)
 
-	lights := light.NewPoint(&math32.Color{R: 1, G: 1, B: 1}, 20.0)
-	lights.SetPosition(0, 0, 0)
+	lights := light.NewPoint(&math32.Color{R: 1, G: 1, B: 1}, 30.0)
+	lights.SetPosition(4.0, 3.0, -2.0)
 	system.Add(lights)
 
 	a.Run(func(renderer *renderer.Renderer, deltaTime time.Duration) {
